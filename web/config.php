81d47893d4dc4cda6a8f6b6c388eb5e44d276245
hkhx2009xiao <43689758+hkhx2009xiao@users.noreply.github.com>
2018-10-01 13:21:03 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
-define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('SITE_NAME', 'hkhx2009xiao');
+define('TITLENAME', 'YouTube');
+define('EN2DEKEY', 'SM0omxXC8Smpg46F');
+define('EMAIL', 'hkhx2009xiaohuangyan@gmail.com');
 ?>

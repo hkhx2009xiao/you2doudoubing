aaa1a6f46351671ada9583b76370e2fd68c67f6a
hkhx2009xiao <43689758+hkhx2009xiao@users.noreply.github.com>
2018-10-01 23:20:05 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
+define('APIKEY', 'AIzaSyCWcEVoFxB8vTcUbtoN1mHgsSkBchOssqM');
 define('GJ_CODE', 'US');
 define('SITE_NAME', 'YouTube');
 define('TITLENAME', 'YouTube');
 define('EN2DEKEY', 'SM0omxXC8Smpg46F');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'hkhx2009xiaohuangyan@gmail.com');
 ?>

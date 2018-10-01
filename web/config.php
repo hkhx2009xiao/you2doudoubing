fbd7447c41d7440cbe20bf04fa6aa052070026bd
hkhx2009xiao <43689758+hkhx2009xiao@users.noreply.github.com>
2018-10-01 18:14:07 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,7 +2,7 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCWcEVoFxB8vTcUbtoN1mHgsSkBchOssqM');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', 'hkhx2009xiao');
+define('SITE_NAME', 'YouTube');
 define('TITLENAME', 'YouTube');
 define('EN2DEKEY', 'SM0omxXC8Smpg46F');
 define('EMAIL', 'hkhx2009xiaohuangyan@gmail.com');
